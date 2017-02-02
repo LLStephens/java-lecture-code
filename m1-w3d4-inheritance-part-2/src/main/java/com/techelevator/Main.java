4a8cbd24b49c6d8bf2a763d37049244c23a17dea
Casey Borders <casey@techelevator.com>
2017-02-02 10:15:55 -0500

    Adding inheritance part 2

---
+++ m1-w3d4-inheritance-part-2/src/main/java/com/techelevator/Main.java
@@ -0,0 +1,11 @@
+package com.techelevator;
+
+public class Main {
+	public static void main(String[] args) {
+//		Game game = new Checkers();
+//		game.playOneGame();
+		
+		Game otherGame = new Monopoly(4);
+		otherGame.playOneGame();
+	}
+}
